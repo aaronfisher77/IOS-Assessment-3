7de9e24e6755b2a137f9d425bef0471080e8e0cc
Aaron Fisher <aaronfisher@MacBook-Pro-20.local>
2018-11-08 10:07:47 -0600

    Initial Commit

---
+++ Week3Assessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week3Assessment
+//
+//  Created by Aaron Fisher on 11/8/18.
+//  Copyright © 2018 Aaron Fisher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
